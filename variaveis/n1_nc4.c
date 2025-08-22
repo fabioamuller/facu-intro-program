b2a622b4e252c39ef2fa78b98fa30df95f33da68
Fabio Muller <f.famuller@gmail.com>
2025-08-22 18:04:34 -0300

    ok

--- variaveis/n1_nc4.c
+++ variaveis/n1_nc4.c
@@ -4,6 +4,10 @@ int main() {
     int idade = 0;
     char nome[60] = "fabio";
 
+    printf("Qual a sua idade? ");
+    scanf("%d", &idade);
+    printf("Qual seu nome? ");
+    scanf("%s", &nome);
     printf("teste de idade: %d\n", idade);
     printf("nome: %s\n", nome);
 
